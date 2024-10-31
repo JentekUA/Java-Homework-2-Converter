e73a2d722f68effa79879f345375068ebd1fdf4c
oleksandr.aleksashyn <oleksandr.aleksashyn@delasport.com>
2024-10-31 16:41:00 +0200

    Add logic for miles to kilometers conversion

--- src/app/Main.java
+++ src/app/Main.java
@@ -2,8 +2,18 @@ package app;
 
 public class Main {
 
+    private static final double CONV_K = 1.60934;
+
     public static void main(String[] args) {
         System.out.println("App for measures converting.");
         System.out.println("Version 1.0.");
+
+        double miles = 5;
+        double convertedMiles = convMilesToKilometers(miles);
+        System.out.println("Result is " + convertedMiles + " miles.");
+    }
+
+    private static double convMilesToKilometers(double miles) {
+        return miles * CONV_K;
     }
 }
